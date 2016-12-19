ad2a5156b4f0656f3b8482ccc95aa50369bc1451
beverts312 <texas312@gmail.com>
2016-12-18 20:05:35 -0600

    add unit tests for get hosted zone id

---
+++ test/lib/dns-utils-tests.ts
@@ -0,0 +1,81 @@
+'use strict';
+
+import Chai = require('chai');
+import Sinon = require('sinon');
+import AWS = require('aws-sdk');
+
+import DnsUtils = require('../../src/lib/dns-utils');
+
+const assert = Chai.assert;
+
+suite('Helper Suite -', () => {
+    let sut: DnsUtils;
+
+    suite('getHostedZoneId Suite -', () => {
+        let sandbox: Sinon.SinonSandbox;
+        let listStub: Sinon.SinonStub;
+
+        suiteSetup(() => {
+            sandbox = Sinon.sandbox.create();
+            const t = { listHostedZones: () => { } };
+            listStub = Sinon.stub(t, 'listHostedZones');
+            Sinon.stub(AWS, 'Route53').returns(t);
+            sut = new DnsUtils();
+        });
+
+        suiteTeardown(() => {
+            sandbox.restore();
+        });
+
+        test('Handles error response - ', (done: () => void) => {
+            listStub.throws(new Error());
+            sut.getHostedZoneId('').catch((err) => {
+                assert.isNotNull(err);
+                assert.isTrue(listStub.called);
+                done();
+            });
+        });
+
+        test('Handles Missing Zone -', (done: () => void) => {
+            const data: AWS.Route53.ListHostedZonesResponse = {
+                HostedZones: [
+                    {
+                        Id: 'id',
+                        Name: 'name',
+                        CallerReference: ''
+                    }
+                ],
+                Marker: '',
+                IsTruncated: false,
+                MaxItems: ''
+            };
+            listStub.yields(null, data);
+            sut.getHostedZoneId('test').catch((err) => {
+                assert.equal(err.message, 'Could Not Find Record');
+                assert.isTrue(listStub.called);
+                done();
+            });
+        });
+
+        test('Retrieves Correct ID -', (done: () => void) => {
+            const data: AWS.Route53.ListHostedZonesResponse = {
+                HostedZones: [
+                    {
+                        Id: 'id',
+                        Name: 'name',
+                        CallerReference: ''
+                    }
+                ],
+                Marker: '',
+                IsTruncated: false,
+                MaxItems: ''
+            };
+            listStub.yields(null, data);
+            sut.getHostedZoneId('name').then((id) => {
+                assert.equal(id, 'id');
+                assert.isTrue(listStub.called);
+                done();
+            });
+        });
+    });
+});
